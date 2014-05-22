9b76df5102e4ecc30519a9fba2e3c417fb927f29
Kabootit <kabootit@users.noreply.github.com>
2014-05-22 09:59:50 -0400

    put access check into every api method
    part 1, contains old (better) version also

--- test_direct_api.js
+++ test_direct_api.js
@@ -1,1 +1,1 @@
-﻿/*** TEST: do something with API locally (from same project)* use commonJS approach*/var AC = require(FileSystemSync("PROJECT").path + "API/AC.js");// good callvar x = AC.organization.getAll();x;// restricted call//var y = AC.organization.removeAll();//y;
+﻿/*** TEST: do something with API locally (from same project)* use commonJS approach*/var AC = require(FileSystemSync("PROJECT").path + "API/AC_simple.js");// good callvar x = AC.organization.getAll();x;// restricted call//var y = AC.organization.removeAll();//y;
