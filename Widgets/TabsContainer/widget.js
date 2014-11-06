6e71be04bd11cbe34dbea6c36cc8e6d5d8c973dd
Kabootit <kabootit@users.noreply.github.com>
2014-11-06 12:34:51 -0500

    cleanup

--- Widgets/TabsContainer/widget.js
+++
@@ -1,9 +0,0 @@
-WAF.define('TabsContainer', ['waf-core/widget'], function(widget) {
-    "use strict";
-
-    var TabsContainer = widget.createSubWidget('TabsContainer');
-    TabsContainer.inherit('waf-behavior/layout/container');
-    TabsContainer.addClass('waf-ui-box');
-
-    return TabsContainer;
-});
