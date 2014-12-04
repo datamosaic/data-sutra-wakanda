47e994fe51a901d3562d7b66982372a1c1b40867
Kabootit <kabootit@users.noreply.github.com>
2014-12-04 11:23:31 -0500

    refactor rpc-proxy location

--- test_call_api_local.js
+++ test_call_api_local.js
@@ -1,1 +1,1 @@
-﻿// NEW NEW WAY//var SOLUTION = require(FileSystemSync("EXTENSIONS_USER").path + 'Kabootit/solution.js');var results = SOLUTION.callAPI('AC.organization.getAll');//var results = FACTORY.external('AC.organization.getAll',[1,'string',{fish: 'bowl'}]);results
+// NEW NEW WAY//var KB = require(FileSystemSync("EXTENSIONS_USER").path + 'Kabootit/solution.js');var results = KB.callAPI('AC.organization.getAll');//var results = FACTORY.external('AC.organization.getAll',[1,'string',{fish: 'bowl'}]);results
