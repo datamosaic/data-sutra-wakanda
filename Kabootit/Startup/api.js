39f33c5d7a61a1570397334abbfc352c8a571121
Kabootit <kabootit@users.noreply.github.com>
2014-12-11 16:19:54 -0500

    correct api url

--- Kabootit/Startup/api.js
+++ Kabootit/Startup/api.js
@@ -1,1 +1,1 @@
-// external project API-accessaddHttpRequestHandler(      '(?:/kb-code)?/api\.\*',      'Kabootit/API/code.js',       'acController' );
+// external project API-accessaddHttpRequestHandler(      '(?:/kb-code)?/api\.\*',      'Kabootit/API/code.js',       'codeController' );
