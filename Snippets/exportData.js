768f35de3605e44232b2cf3102b27e025facd6f2
Kabootit <kabootit@users.noreply.github.com>
2015-03-10 18:48:37 -0400

    use highest version of api available to tell if actions allowed

---
+++ Snippets/exportData.js
@@ -0,0 +1,1 @@
+﻿// get a reference to the export foldervar myFolder = Folder("/Volumes/Ice Man/stratos/navigation");// if the folder actually existsif (!myFolder.exists) {	myFolder.create();}// exportds.exportAsJSON( myFolder ) ;
