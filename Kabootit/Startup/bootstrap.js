ba0ce24d7f42ccc776cc1f7612289b0fcbb8abd1
Kabootit <kabootit@users.noreply.github.com>
2015-03-18 15:09:16 -0400

    add in our requestHandlers on first request to project

---
+++ Kabootit/Startup/bootstrap.js
@@ -0,0 +1,1 @@
+﻿// first request that comes will instantiate all project request handlers before serving up requestaddHttpRequestHandler(	'^',	Folder(ds.getModelFolder().path).parent.path + 'Resources/Extensions/bootstrap/requestHandler.js',	'initialize');
