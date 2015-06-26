5acc8c87b5467b86156d8ab6606666120d1c08d7
Kabootit <kabootit@users.noreply.github.com>
2015-06-26 11:26:14 -0400

    put tabs in security screen

---
+++ WebFolder/navigation.waPage/scripts/index.js
@@ -0,0 +1,32 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btn_user = {};	// @menuItem
+	var btn_group = {};	// @menuItem
+	var btn_navigation = {};	// @menuItem
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btn_user.click = function btn_user_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+	btn_group.click = function btn_group_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+	btn_navigation.click = function btn_navigation_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("btn_user", "click", btn_user.click, "WAF");
+	WAF.addListener("btn_group", "click", btn_group.click, "WAF");
+	WAF.addListener("btn_navigation", "click", btn_navigation.click, "WAF");
+// @endregion
+};// @endlock
