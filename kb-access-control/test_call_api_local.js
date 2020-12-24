5c04feddbee0ff5a92c808cecd9ab1e78a9e6947
Kabootit <kabootit@users.noreply.github.com>
2020-12-24 13:29:45 -0500

    test line ending change

--- kb-access-control/test_call_api_local.js
+++ kb-access-control/test_call_api_local.js
@@ -1,1 +1,7 @@
-// NEW NEW WAY//var KB = require(FileSystemSync("EXTENSIONS_USER").path + 'Kabootit/solution.js');var results = KB.callAPI('AC.organization.getAll');//var results = FACTORY.external('AC.organization.getAll',[1,'string',{fish: 'bowl'}]);results
+
+// NEW NEW WAY
+//var KB = require(FileSystemSync("EXTENSIONS_USER").path + 'Kabootit/solution.js');
+var results = KB.callAPI('AC.organization.getAll');
+
+//var results = FACTORY.external('AC.organization.getAll',[1,'string',{fish: 'bowl'}]);
+results
